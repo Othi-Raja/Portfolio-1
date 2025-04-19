ca4c961c3f262e8e448b1e3eef6d75dee36bf922
Kumaravel <144203983+Kumaravel4502@users.noreply.github.com>
2025-04-20 00:00:13 +0530

    Update SubSkills.jsx

--- Kumaravel Portfolio/src/components/skillsSection/SubSkills.jsx
+++ Kumaravel Portfolio/src/components/skillsSection/SubSkills.jsx
@@ -3,7 +3,7 @@ const SubSkills = () => {
     <div className="border-y-2 border-lightGrey relative ">
       <div className="absolute bg-gradient-to-r from-orange to-cyan opacity-50 w-full h-full"></div>
       <img
-        src="../../public/images/subSkills.jpg"
+        src="https://ik.imagekit.io/11ie2ifrf/subSkills.jpg"
         alt="subSkills image"
         className=""
       />
