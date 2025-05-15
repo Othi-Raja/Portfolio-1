28107d4223ca25c717b5e3bd8ddf0b0ae437c055
OthiRaja <othiraja5677@gmail.com>
2025-05-15 20:25:39 +0530

    resume

--- src/components/aboutMeSection/AboutMeText.jsx
+++ src/components/aboutMeSection/AboutMeText.jsx
@@ -14,6 +14,7 @@ const AboutMeText = () => {
        <a
         download="Kumaravel_Frontend_Developer_Resume1.pdf"
         href="Kumaravel_Frontend_Developer_Resume1.pdf"
+
         className="border border-orange rounded-full py-2 px-4 text-lg flex gap-2 items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer text-white hover:text-cyan"
       >
         Download CV
